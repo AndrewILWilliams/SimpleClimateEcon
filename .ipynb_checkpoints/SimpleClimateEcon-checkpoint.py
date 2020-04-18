35819d10f937ed2048d52a1613c15e8fc5696d6c
Andrew Williams <williamsa3142@gmail.com>
2020-04-18 23:46:04 +0100

    Fixed bugs in class implementation of model

--- .ipynb_checkpoints/SimpleClimateEcon-checkpoint.py
+++ .ipynb_checkpoints/SimpleClimateEcon-checkpoint.py
@@ -8,14 +8,12 @@ Currently no carbon-cycle feedbacks at play or sequestration tech,
 so can -at best- only get stable temps and not declining temps.
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
 class CBA:
     def __init__(self, W0=75*10**12, D0=0.00267, pback=200, gamma=2,
                  gsig=-0.0125, TCRE=0.00054, ks=0.12, g=0.02, r=0.035,
                  E0=39, T0=0.9):
-        # Imports
-        import numpy as np
-        import matplotlib.pyplot as plt
-        
         # Model parameters
         self.W0    = W0
         self.T0    = T0
@@ -33,41 +31,41 @@ class CBA:
     def increment_T(self, T, dTdt):
         return T+dTdt
 
-    def dTdt(self, E):
+    def calc_dTdt(self, E):
         """
         TCRE relationship between instantaneous emissions 
         and annual rate of change of temperature.
         """
         return self.TCRE*E
     
-    def consumption(self, year):
+    def calc_consumption(self, year):
         return self.W0 * np.exp(self.g*(year-2005))
 
-    def SCC(self, year, T, dTdt):
+    def calc_SCC(self, year, T, dTdt):
         """
         Social Cost of Carbon ($/tCO2)
 
         Derived in text.
         """
-        prefactor=self.gamma*self.TCRE*self.D0*consumption(year)* T**(self.gamma-1)
+        prefactor=self.gamma*self.TCRE*self.D0*self.calc_consumption(year)* T**(self.gamma-1)
         bracket  = np.divide(1, self.kd) - np.divide(1,self.ks+self.kd) 
         + np.divide((self.gamma-1)*dTdt, T)*((1/self.kd)**2 - (1/(self.ks+self.kd))**2)
         return prefactor*bracket*10**-9
 
-    def abatement_rate(self, SCC):
+    def calc_abatement_rate(self, SCC):
         """
         Abatement rate, used in emissions equation.
         E = sigma * (1-abatement_rate) * W
 
         Abatement rate is a function of SCC. **NOT SURE WHAT STUART USES?**
         """
-        test = 0.003*SCC
+        test = 0.005*SCC
         if test >=1:
             return 1
         else:
             return test
 
-    def emissions(self, year, abate, SCC):
+    def calc_emissions(self, year, abate, SCC):
         """
         E = sigma * (1-abatement_rate) * W
 
@@ -76,7 +74,7 @@ class CBA:
         """
 
         sigma = self.sig0 * np.exp(self.gsig*(year-2005)) 
-        W     = self.W_0  * np.exp(self.g*(year-2005))
+        W     = self.W0  * np.exp(self.g*(year-2005))
         return sigma * (1-abate) * W
 
     def initialize(self, nyears):
@@ -98,11 +96,11 @@ class CBA:
         
         """Run main loop"""
         for idx, year in enumerate(self.years):
-            self.dTdt[idx]    = dTdt(self.emiss[idx])
-            self.temps[idx+1] = increment_T(self.temps[idx], self.dTdt[idx])
-            self.SCC[idx]     = SCC(year, self.temps[idx], self.dTdt[idx])
-            self.abate[idx]   = abatement_rate(self.SCC[idx])
-            self.emiss[idx+1] = emissions(year, self.abate[idx], self.SCC[idx])
+            self.dTdt[idx]    = self.calc_dTdt(self.emiss[idx])
+            self.temps[idx+1] = self.increment_T(self.temps[idx], self.dTdt[idx])
+            self.SCC[idx]     = self.calc_SCC(year, self.temps[idx], self.dTdt[idx])
+            self.abate[idx]   = self.calc_abatement_rate(self.SCC[idx])
+            self.emiss[idx+1] = self.calc_emissions(year, self.abate[idx], self.SCC[idx])
 
             #if self.emiss[idx+1]==0:
             #    self.temps[idx+1:]=np.nan
